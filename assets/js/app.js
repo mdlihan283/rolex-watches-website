0bce86ffba75a86024b6986963fb492af2127746
Md Lihan <freelancerlihansocial@gmail.com>
2022-12-16 23:18:38 +0600

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,27 @@ window.onscroll = () => {
     topBar.classList.remove("barActive");
   }
 };
+
+// cart section
+let addCart = document.getElementById("addCart");
+let cartClose = document.getElementById("cartClose");
+let cartSection = document.getElementById("cartSection");
+addCart.addEventListener("click", () => {
+  cartSection.classList.add("cartSecActive");
+});
+
+cartClose.addEventListener("click", () => {
+  cartSection.classList.remove("cartSecActive");
+});
+
+// menu bar
+let mobileBar = document.getElementById("mobileBar");
+let menuClose = document.getElementById("menuBarClose");
+let menu = document.querySelector(".menu");
+mobileBar.addEventListener("click", () => {
+  menu.classList.add("activeMenu");
+});
+
+menuClose.addEventListener("click", () => {
+  menu.classList.remove("activeMenu");
+});
